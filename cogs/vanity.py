9b5f37d055e38834f1ac79f0eca8cc958ce656f2
storch <pianister123@gmail.com>
2024-10-03 22:12:42 -0400

    log vanity changes

--- cogs/vanity.py
+++ cogs/vanity.py
@@ -15,22 +15,36 @@ class Vanity(commands.Cog):
             if activity.type == discord.ActivityType.custom:
                 return f'/{self.invite}' in activity.name.lower()
         return False
+    
+    def get_vanity(self, member):
+        for activity in member.activities:
+            if activity.type == discord.ActivityType.custom:
+                if f'/{self.invite}' in activity.name.lower():
+                    return activity.name
+        return None
 
     @commands.Cog.listener()
     async def on_presence_update(self, before, after):
         if after.guild.id != self.bot.GUILD_ID:
             return 
         
-        if before.status != after.status:
+        if after.status is discord.Status.offline:
             return
 
+        # if before.status != after.status:
+        #     return
+
         if not self.has_vanity(before) and self.has_vanity(after):
+
             role = after.guild.get_role(self.bot.vars.get('vanity-role-id'))
             
             if role in after.roles:
                 return 
 
             await after.add_roles(role)
+
+            with open('vanitylog.txt', '+a') as f:
+                f.write(f'{after} gained vanity: before={self.get_vanity(before)}, after={self.get_vanity(after)}\n')
 
             channel = self.bot.get_channel(self.bot.vars.get('vanity-channel-id'))
             layout = self.bot.get_layout('newrep')
@@ -41,6 +55,9 @@ class Vanity(commands.Cog):
             if role not in after.roles:
                 return
             await after.remove_roles(role)
+
+            with open('vanitylog.txt', '+a') as f:
+                f.write(f'{after} lost vanity: before={self.get_vanity(before)}, after={self.get_vanity(after)}\n')
 
     async def cog_load(self):
         await self.update_roles()
