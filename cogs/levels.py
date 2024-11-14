054d23224ef1917357f0a96afff38ca9f76f151c
storch <pianister123@gmail.com>
2024-11-13 21:50:13 -0500

    11/13 sync 3

--- cogs/levels.py
+++ cogs/levels.py
@@ -1,24 +1,29 @@
-from discord.ext import commands, tasks
-from zoneinfo import ZoneInfo
-import json 
-from num2words import num2words
-from .utils import generate_rank_card, SimplePages, Layout, LayoutContext
-from io import BytesIO
 import time
 import random
-import discord
 import datetime
 import math
-from .utils import next_sunday
+from io import BytesIO
+
+from num2words import num2words
+
+import discord
+from discord import app_commands
+from discord.ext import commands, tasks
+
+from .utils import (
+    generate_rank_card,
+    next_sunday,
+    SimplePages,
+    Layout,
+    LayoutContext,
+)
 
 
 def get_xp(lvl: int):
     return 50*lvl*lvl 
 
-
 def get_level(xp: int):
     return int(math.sqrt(xp / 50))
-
 
 class Levels(commands.Cog):
 
@@ -50,20 +55,22 @@ class Levels(commands.Cog):
             1191558432713625812,
             1193061987240910898,
         ]
-        self.xp_multipliers = {
-            913086743035658292: 1.2,
-            1060750259980079195: 1.15,
-            981252193132884109: 1.1
-        }
-        self.xp_cache = {} 
-        self.bot.xp_cache = self.xp_cache
-        self.msg_counts = {}
+        self.xp_multipliers = (
+            (913086743035658292, 1.2),
+            (1060750259980079195, 1.15),
+            (981252193132884109, 1.1),
+        )
+        # self.xp_cache = {} 
+        # self.bot.xp_cache = self.xp_cache
+        # self.msg_counts = {}
         self.weekly_xp_task.start()
+        self.cleanup_cooldowns.start()
 
     # make a loop that runs every sunday at 1am 
-    async def weekly_xp(self):
+    async def weekly_xp(self, *, channel=None, reset=False):
         # await self.dump_xp()
-        query = '''SELECT xp.user_id, (xp.total_xp - xp_copy.total_xp) AS diff
+        query = '''
+                    SELECT xp.user_id, (xp.total_xp - xp_copy.total_xp) AS diff
                     FROM xp
                     INNER JOIN xp_copy ON xp.user_id = xp_copy.user_id
                     WHERE xp.user_id != $1 
@@ -72,104 +79,92 @@ class Levels(commands.Cog):
                 '''
         rows = await self.bot.db.fetch(query, self.bot.vars.get('luna-id'))
     
-        # TODO: make this a layout
-        with open('cogs/static/embeds.json', encoding='utf8') as f:
-            data = json.load(f)
-        
-        embed = discord.Embed.from_dict(data['weekly_lb'])
+        lb_layout = self.bot.get_layout('weeklylb')
+        lb_repls = {}
 
         for i in range(3):
-            try:
-                msgs = self.msg_counts[rows[i][0]]
-            except KeyError:
-                msgs = 'placeholder'
-            embed.description = embed.description.replace(f'{{ping{i}}}', f'<@{rows[i][0]}>')
-            embed.description = embed.description.replace(f'{{xp{i}}}', f'{rows[i][1]}')
-            embed.description = embed.description.replace(f'{{msgs{i}}}', f'{msgs}')
+            row = rows[i]
+            user_id = row['user_id']
+            mention = f'<@{user_id}>'
+            query = 'SELECT count FROM msg_count WHERE user_id = $1'
+            msg_count = await self.bot.db.fetchval(query, row["user_id"])   
+            xp = row['diff']
+
+            lb_repls[f'mention{i+1}'] = mention
+            lb_repls[f'xp{i+1}'] = xp
+            lb_repls[f'messages{i+1}'] = msg_count
         
-        embed2 = discord.Embed.from_dict(data['weekly_reset'])
         this_sunday = discord.utils.utcnow()
         next_sunday = this_sunday + datetime.timedelta(days=7)
-        embed2.description = embed2.description.replace('{this_sunday}', discord.utils.format_dt(this_sunday, 'd'))
-        embed2.description = embed2.description.replace('{next_sunday}', discord.utils.format_dt(next_sunday, 'd'))
 
-        channel = self.bot.get_channel(1137942143562940436)
-        await channel.send(embed=embed)
-        await channel.send(embed=embed2)
-        
-        await self.freeze_lb()
+        reset_layout = self.bot.get_layout('weeklyreset')
+        reset_repls = {
+            'thissunday': discord.utils.format_dt(this_sunday, 'd'),
+            'nextsunday': discord.utils.format_dt(next_sunday, 'd'),
+        }
+
+        if channel is None:
+            channel = self.bot.get_var_channel('weekly-reset')
+
+        await lb_layout.send(channel, repls=lb_repls)
+        await reset_layout.send(channel, repls=reset_repls)
+
+        if reset: 
+            await self.reset_lb()
 
     @tasks.loop(hours=168)
     async def weekly_xp_task(self):
-        await self.weekly_xp()
+        await self.weekly_xp(reset=True)
         # select top 3 users order by the difference between xp and xp_copy 
             
     @weekly_xp_task.before_loop
     async def before_weekly_xp(self):
         await discord.utils.sleep_until(next_sunday())
 
-    async def cog_load(self):
-        query = 'select user_id, total_xp from xp'
-        rows = await self.bot.db.fetch(query)
-        for row in rows:
-            self.xp_cache[row['user_id']] = row['total_xp']
-        rows = await self.bot.db.fetch('select user_id, count from msg_count')
-        for row in rows:
-            self.msg_counts[row['user_id']] = row['count']
+    # async def cog_load(self):
+    #     query = 'select user_id, total_xp from xp'
+    #     rows = await self.bot.db.fetch(query)
+        # for row in rows:
+        #     self.xp_cache[row['user_id']] = row['total_xp']
+        # rows = await self.bot.db.fetch('select user_id, count from msg_count')
+        # for row in rows:
+        #     self.msg_counts[row['user_id']] = row['count']
 
-    # async def dump_xp(self):
-    #     for user_id, total_xp in self.xp_cache.items():
-    #         query = '''INSERT INTO xp (user_id, "total_xp") 
-    #                     VALUES ($1, $2)
-    #                     ON CONFLICT (user_id)
-    #                     DO UPDATE SET total_xp = $2
-    #                 '''
-    #         await self.bot.db.execute(query, user_id, total_xp) 
-        
-    # async def dump_msg_counts(self):
-    #     for user_id, count in self.msg_counts.items():
-    #         query = '''INSERT INTO msg_count (user_id, count) 
-    #                     VALUES ($1, $2)
-    #                     ON CONFLICT (user_id)
-    #                     DO UPDATE SET count = $2
-    #                 '''
-    #         await self.bot.db.execute(query, user_id, count)
+    @tasks.loop(minutes=10)  # Runs every 10 minutes; adjust as needed
+    async def cleanup_cooldowns(self):
+        """Task to clean up expired entries from xp_cooldowns."""
+        current_time = time.time()
+        # Remove entries where the cooldown time has passed
+        self.xp_cooldowns = {user_id: cooldown for user_id, cooldown in self.xp_cooldowns.items() if cooldown > current_time}
 
     async def cog_unload(self):
         self.weekly_xp_task.cancel()
-        # await self.dump_xp()
-        # await self.dump_msg_counts()
-    
-    async def freeze_lb(self):
+        self.cleanup_cooldowns.cancel()
+
+    async def reset_lb(self):
         await self.bot.db.execute('DROP TABLE IF EXISTS xp_copy')
         # make a copy of the xp table
         await self.bot.db.execute('CREATE TABLE xp_copy AS SELECT * FROM xp')
         await self.bot.db.execute('DROP TABLE IF EXISTS msg_count')
         await self.bot.db.execute('CREATE TABLE msg_count (user_id BIGINT PRIMARY KEY, count INTEGER)')
-        self.msg_counts = {}
+        # self.msg_counts = {}
 
-    async def add_leveled_roles(self, message, old_level, new_level, authorroles):
-        roles = {lvl: self.leveled_roles[lvl] for lvl in self.leveled_roles if lvl <= new_level}
+    async def add_leveled_roles(self, message, old_level, new_level):
+        roles = {lvl: message.guild.get_role(self.leveled_roles[lvl]) for lvl in self.leveled_roles if lvl <= new_level}
         if roles:
             keys = list(roles.keys())
             lvl = max(keys)
             role = roles[lvl]
             keys.remove(lvl)
-            if role not in authorroles:
-                role = message.guild.get_role(role)
+
+            if role not in message.author.roles:
                 await message.author.add_roles(role)
             for lvl in keys:
-                role = roles[lvl]
-                if role in authorroles:
-                    role = message.guild.get_role(role)
+                role = message.guild.get_role(roles[lvl])
+                if role in message.author.roles:
                     await message.author.remove_roles(role)
 
         if old_level != new_level:
-            # embed = discord.Embed(title='❀ㆍㆍLevel Up﹗⁺ ₍ <a:LC_lilac_heart_NF2U_DNS:1046191564055138365> ₎', color=0xcab7ff)
-            # embed.description = f'> ♡﹒﹒**Psst!** Tysm for being active here with us, you are now level {new_level}. Keep sending messages to gain more levels, which can gain you some **epic perks**. Tired of receiving these level up messages?? Go [here](https://discord.com/channels/899108709450543115/1106225161562230925) to remove access to this channel; just react to that message again to regain access. <a:LC_star_burst:1147790893064142989> ✿❀'
-            # embed.set_footer(text='⁺﹒Type ".myperks" to view our full list of available perks, including perks for our active members﹒⁺')
-            # channel = self.bot.get_channel(1137942143562940436)
-            # await channel.send(f'⁺﹒{message.author.mention}﹗𖹭﹒⁺', embed=embed)
             channel = self.bot.get_var_channel('levelup')
             layout = self.bot.get_layout('levelup')
             ctx = LayoutContext(author=message.author) 
@@ -177,17 +172,31 @@ class Levels(commands.Cog):
 
         self.xp_cooldowns[message.author.id] = time.time() + 15
 
-    async def get_xp_info(self, user):
-        query = """SELECT (
-                       SELECT COUNT(*)
-                       FROM xp second
-                       WHERE second.total_xp >= first.total_xp
-                   ) AS rank, total_xp 
-                   FROM xp first
-                   WHERE user_id = $1
-                """
-        res = await self.bot.db.fetchrow(query, user.id)
-        return res
+    # async def get_xp_info(self, user):
+    #     query = """SELECT (
+    #                    SELECT COUNT(*)
+    #                    FROM xp second
+    #                    WHERE second.total_xp >= first.total_xp
+    #                    AND second.user_id != $2
+    #                ) AS rank, total_xp 
+    #                FROM xp first
+    #                WHERE user_id = $1
+    #             """
+    #     row = await self.bot.db.fetchrow(query, user.id, self.bot.vars.get('luna-id'))
+    #     return row
+
+    def get_increment(self, member):
+        increment = random.randint(20, 25)
+
+        # If it's a weekend, multiply by 1.2
+        if datetime.datetime.today().weekday() in (5, 6):
+            increment *= 1.2
+
+        for role_id, multi in self.xp_multipliers:
+            if member.guild.get_role(role_id) in member.roles:
+                increment *= multi 
+
+        return round(increment)
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -199,43 +208,42 @@ class Levels(commands.Cog):
         if message.channel.id not in self.whitelisted_channels or message.author.bot:
             return 
 
-        self.msg_counts[message.author.id] = self.msg_counts.get(message.author.id, 0) + 1
-        count = self.msg_counts[message.author.id]
+        # self.msg_counts[message.author.id] = self.msg_counts.get(message.author.id, 0) + 1
+        # count = self.msg_counts[message.author.id]
 
         query = '''INSERT INTO msg_count (user_id, count) 
                     VALUES ($1, $2)
                     ON CONFLICT (user_id)
-                    DO UPDATE SET count = $2
+                    DO UPDATE SET count = msg_count.count + $2
                 '''
-        await self.bot.db.execute(query, message.author.id, count)
+        await self.bot.db.execute(query, message.author.id, 1)
 
         if message.author.id not in self.xp_cooldowns or self.xp_cooldowns[message.author.id] < time.time():
-            authorroles = [role.id for role in message.author.roles]
-            increment = random.randint(20, 25)
-            for role_id, multi in self.xp_multipliers.items():
-                if role_id in authorroles:
-                    increment *= multi 
-            increment = round(increment)
-            old = self.xp_cache.get(message.author.id)
-            if old is None:
-                self.xp_cache[message.author.id] = increment 
-                new = increment
-                old = 0
-            else:
-                self.xp_cache[message.author.id] += increment
-                new = old + increment 
+            increment = self.get_increment(message.author)
+
+            # old = self.xp_cache.get(message.author.id)
+            # if old is None:
+            #     self.xp_cache[message.author.id] = increment 
+            #     new = increment
+            #     old = 0
+            # else:
+            #     self.xp_cache[message.author.id] += increment
+            #     new = old + increment 
 
             query = '''INSERT INTO xp (user_id, "total_xp") 
                         VALUES ($1, $2)
                         ON CONFLICT (user_id)
-                        DO UPDATE SET total_xp = $2
+                        DO UPDATE SET total_xp = xp.total_xp + $2
+                        RETURNING total_xp
                     '''
-            await self.bot.db.execute(query, message.author.id, new) 
+            new_xp = await self.bot.db.fetchval(query, message.author.id, increment) 
+            old_xp = new_xp - increment
 
-            new_level, old_level = get_level(new), get_level(old)
-            await self.add_leveled_roles(message, old_level, new_level, authorroles)
+            new_level, old_level = get_level(new_xp), get_level(old_xp)
+            await self.add_leveled_roles(message, old_level, new_level)
 
     @commands.hybrid_command(name='rank')
+    @app_commands.describe(member='The member to check the rank of.')
     async def _rank(self, ctx, *, member: discord.Member = None):
         """Checks your server level and XP and other stats"""
 
@@ -243,19 +251,32 @@ class Levels(commands.Cog):
 
         async with ctx.channel.typing():
 
-            xp = self.xp_cache.get(m.id)
-            if xp is None:
-                self.xp_cache[ctx.author.id] = 0
-                xp = 0
+            # xp = self.xp_cache.get(m.id)
+            # if xp is None:
+            #     self.xp_cache[ctx.author.id] = 0
+            #     xp = 0
 
-            xps_no_luna = [v for k, v in self.xp_cache.items() if k != self.bot.vars.get('luna-id')] 
-            rank = len([v for v in xps_no_luna if v > xp]) + 1
-            mx = xp
-            current_level = get_level(mx)
+            # xps_no_luna = [v for k, v in self.xp_cache.items() if k != self.bot.vars.get('luna-id')] 
+            # rank = len([v for v in xps_no_luna if v > xp]) + 1
+
+            query = "SELECT total_xp FROM xp WHERE user_id = $1"
+            xp = await self.bot.db.fetchval(query, m.id)
+
+            # Select all rows that have higher xp than the user
+            query = "SELECT user_id FROM xp WHERE total_xp > $1 AND user_id != $2"
+            rows = await self.bot.db.fetch(query, xp, self.bot.vars.get('luna-id'))
+
+            rank = 1
+            for row in rows:
+                member = ctx.guild.get_member(row['user_id'])
+                if member is not None:
+                    rank += 1
+
+            current_level = get_level(xp)
 
             empty = get_xp(current_level)
             full = get_xp(current_level+1)
-            pc = (mx - empty) / (full - empty)
+            pc = (xp - empty) / (full - empty)
 
             av_file = BytesIO()
             await m.display_avatar.with_format('png').save(av_file)
@@ -268,27 +289,24 @@ class Levels(commands.Cog):
             embed = Layout.fill_embed(embed, {
                 'ordinal': num2words(rank, to='ordinal_num'),
                 'totalxp': xp,
-                'neededxp': full - mx
+                'neededxp': full - xp
             }, special=False)
             await ctx.send(embed=embed, file=discord.File(fp=file, filename='rank.gif'))
-
-#             embed = discord.Embed(title='❀ㆍㆍYour Rank﹗⁺ ₍ <a:LCD_flower_spin:1147757953064128512> ₎', color=0xcab7ff)
-#             embed.description = (f'''
-# > ⁺ <a:Lumi_arrow_R:927733713163403344>﹒__Rank__ :: {num2words(rank, to='ordinal_num')}﹒⁺
-# > ⁺ <a:Lumi_arrow_R:927733713163403344>﹒__XP__ :: {xp}﹒⁺
-# > ⁺ <a:Lumi_arrow_R:927733713163403344>﹒__Needed XP__ :: {full - mx}﹒⁺
-#             ''')
-#             embed.set_footer(text='⁺﹒Type ".myperks" to view our full list of available perks, including perks for our active members﹒⁺')
-#             total = t2 - t1 
-#             await ctx.send(f'Render time: `{round(total, 3)}s`', embed=embed, file=discord.File(fp=file, filename='rank.gif'))
-
 
     @commands.command(aliases=['leaaderboard'])
     async def lb(self, ctx):
         """Shows the XP leaderboard."""
 
-        pairs_no_luna = [pair for pair in self.xp_cache.items() if pair[0] != self.bot.vars.get('luna-id')]
-        pairs = sorted(pairs_no_luna, key=lambda x: x[1], reverse=True)
+        # pairs_no_luna = [pair for pair in self.xp_cache.items() if pair[0] != self.bot.vars.get('luna-id')]
+        # pairs = sorted(pairs_no_luna, key=lambda x: x[1], reverse=True)
+
+        # Get pairs from db
+        query = '''SELECT user_id, total_xp
+                     FROM xp
+                     WHERE user_id != $1
+                     ORDER BY total_xp DESC
+                 '''
+        pairs = await self.bot.db.fetch(query, self.bot.vars.get('luna-id'))
 
         i = 0
         while i < len(pairs):
@@ -300,11 +318,11 @@ class Levels(commands.Cog):
                 i += 1
 
         entries = []
-        for row in pairs:
-            member, xp = ctx.guild.get_member(row[0]), row[1]
-            lvl = get_level(xp)
+        for user_id, total_xp in pairs:
+            member = ctx.guild.get_member(user_id)
+            lvl = get_level(total_xp)
             if member is not None:
-                entries.append(f'{member.mention}\n**Level:** `{lvl}`\n**Total XP:** `{xp}`')
+                entries.append(f'{member.mention}\n**Level:** `{lvl}`\n**Total XP:** `{total_xp}`')
 
         embed = discord.Embed(title=f'XP Leaderboard', color=self.bot.DEFAULT_EMBED_COLOR)
         view = SimplePages(entries, ctx=ctx, embed=embed)
@@ -313,13 +331,13 @@ class Levels(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def givexp(self, ctx, member: discord.Member, xp: int):
-        self.xp_cache[member.id] += xp
+        # self.xp_cache[member.id] += xp
         query = '''INSERT INTO xp (user_id, "total_xp") 
                     VALUES ($1, $2)
                     ON CONFLICT (user_id)
-                    DO UPDATE SET total_xp = $2
+                    DO UPDATE SET total_xp = xp.total_xp + $2
                 '''
-        await self.bot.db.execute(query, member.id, self.xp_cache[member.id])
+        await self.bot.db.execute(query, member.id, xp)
         await ctx.send(f'Gave {xp} xp to {member.mention}.')
 
 async def setup(bot):
