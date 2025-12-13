19e811095a89b37b427d5c015c6c58c5b00b9065
storchdev <pianister123@gmail.com>
2025-12-13 09:12:46 -0500

    use layout, add license

--- cogs/help.py
+++ cogs/help.py
@@ -1,289 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Modified version of the PaginatedHelpCommand and helper classes, from RoboDanny, source/credits:
+# https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/meta.py
+
 import itertools
-import traceback
-from typing import Any, Optional, Union
+from typing import Any, Optional, Union, TYPE_CHECKING
 
 import discord
 from discord.ext import commands, menus
 from discord.ext.commands import Context
 
+from cogs.utils import RoboPages
 
-class NumberedPageModal(discord.ui.Modal, title="Go to page"):
-    page = discord.ui.TextInput(
-        label="Page", placeholder="Enter a number", min_length=1
-    )
-
-    def __init__(self, max_pages: Optional[int]) -> None:
-        super().__init__()
-        if max_pages is not None:
-            as_string = str(max_pages)
-            self.page.placeholder = f"Enter a number between 1 and {as_string}"
-            self.page.max_length = len(as_string)
-
-    async def on_submit(self, interaction: discord.Interaction) -> None:
-        self.interaction = interaction
-        self.stop()
-
-
-class RoboPages(discord.ui.View):
-    def __init__(
-        self,
-        source: menus.PageSource,
-        *,
-        ctx: Context,
-        check_embeds: bool = True,
-        compact: bool = False,
-    ):
-        super().__init__()
-        self.source: menus.PageSource = source
-        self.check_embeds: bool = check_embeds
-        self.ctx: Context = ctx
-        self.message: Optional[discord.Message] = None
-        self.current_page: int = 0
-        self.compact: bool = compact
-        self.clear_items()
-        self.fill_items()
-
-    def fill_items(self) -> None:
-        if not self.compact:
-            self.numbered_page.row = 1
-            self.stop_pages.row = 1
-
-        if self.source.is_paginating():
-            max_pages = self.source.get_max_pages()
-            use_last_and_first = max_pages is not None and max_pages >= 2
-            if use_last_and_first:
-                self.add_item(self.go_to_first_page)
-            self.add_item(self.go_to_previous_page)
-            if not self.compact:
-                self.add_item(self.go_to_current_page)
-            self.add_item(self.go_to_next_page)
-            if use_last_and_first:
-                self.add_item(self.go_to_last_page)
-            if not self.compact:
-                self.add_item(self.numbered_page)
-            self.add_item(self.stop_pages)
-
-    async def _get_kwargs_from_page(self, page: int):
-        value = await discord.utils.maybe_coroutine(self.source.format_page, self, page)
-        if isinstance(value, dict):
-            return value
-        elif isinstance(value, str):
-            return {"content": value, "embed": None}
-        elif isinstance(value, discord.Embed):
-            return {"embed": value, "content": None}
-        else:
-            return {}
-
-    async def show_page(
-        self, interaction: discord.Interaction, page_number: int
-    ) -> None:
-        page = await self.source.get_page(page_number)
-        self.current_page = page_number
-        kwargs = await self._get_kwargs_from_page(page)
-        self._update_labels(page_number)
-        if kwargs:
-            if interaction.response.is_done():
-                if self.message:
-                    await self.message.edit(**kwargs, view=self)
-            else:
-                await interaction.response.edit_message(**kwargs, view=self)
-
-    def _update_labels(self, page_number: int) -> None:
-        self.go_to_first_page.disabled = page_number == 0
-        if self.compact:
-            max_pages = self.source.get_max_pages()
-            self.go_to_last_page.disabled = (
-                max_pages is None or (page_number + 1) >= max_pages
-            )
-            self.go_to_next_page.disabled = (
-                max_pages is not None and (page_number + 1) >= max_pages
-            )
-            self.go_to_previous_page.disabled = page_number == 0
-            return
-
-        self.go_to_current_page.label = str(page_number + 1)
-        self.go_to_previous_page.label = str(page_number)
-        self.go_to_next_page.label = str(page_number + 2)
-        self.go_to_next_page.disabled = False
-        self.go_to_previous_page.disabled = False
-        self.go_to_first_page.disabled = False
-
-        max_pages = self.source.get_max_pages()
-        if max_pages is not None:
-            self.go_to_last_page.disabled = (page_number + 1) >= max_pages
-            if (page_number + 1) >= max_pages:
-                self.go_to_next_page.disabled = True
-                self.go_to_next_page.label = "…"
-            if page_number == 0:
-                self.go_to_previous_page.disabled = True
-                self.go_to_previous_page.label = "…"
-
-    async def show_checked_page(
-        self, interaction: discord.Interaction, page_number: int
-    ) -> None:
-        max_pages = self.source.get_max_pages()
-        try:
-            if max_pages is None:
-                # If it doesn't give maximum pages, it cannot be checked
-                await self.show_page(interaction, page_number)
-            elif max_pages > page_number >= 0:
-                await self.show_page(interaction, page_number)
-        except IndexError:
-            # An error happened that can be handled, so ignore it.
-            pass
-
-    async def interaction_check(self, interaction: discord.Interaction) -> bool:
-        if interaction.user and interaction.user.id in (
-            self.ctx.bot.owner_id,
-            self.ctx.author.id,
-        ):
-            return True
-        await interaction.response.send_message(
-            "This pagination menu cannot be controlled by you, sorry!", ephemeral=True
-        )
-        return False
-
-    async def on_timeout(self) -> None:
-        if self.message:
-            await self.message.edit(view=None)
-
-    async def on_error(
-        self, interaction: discord.Interaction, error: Exception, item: discord.ui.Item
-    ) -> None:
-        if interaction.response.is_done():
-            await interaction.followup.send(
-                "An unknown error occurred, sorry", ephemeral=True
-            )
-        else:
-            await interaction.response.send_message(
-                "An unknown error occurred, sorry", ephemeral=True
-            )
-
-        try:
-            exc = "".join(
-                traceback.format_exception(
-                    type(error), error, error.__traceback__, chain=False
-                )
-            )
-            embed = discord.Embed(
-                title=f"{self.source.__class__.__name__} Error",
-                description=f"```py\n{exc}\n```",
-                timestamp=interaction.created_at,
-                colour=0xCAB7FF,
-            )
-            embed.add_field(
-                name="User", value=f"{interaction.user} ({interaction.user.id})"
-            )
-            embed.add_field(
-                name="Guild", value=f"{interaction.guild} ({interaction.guild_id})"
-            )
-            embed.add_field(
-                name="Channel",
-                value=f"{interaction.channel} ({interaction.channel_id})",
-            )
-            await self.ctx.bot.stats_webhook.send(embed=embed)
-        except discord.HTTPException:
-            pass
-
-    async def start(
-        self, *, content: Optional[str] = None, ephemeral: bool = False
-    ) -> None:
-        if (
-            self.check_embeds
-            and not self.ctx.channel.permissions_for(self.ctx.me).embed_links
-        ):  # type: ignore
-            await self.ctx.send(
-                "Bot does not have embed links permission in this channel.",
-                ephemeral=True,
-            )
-            return
-
-        await self.source._prepare_once()
-        page = await self.source.get_page(0)
-        kwargs = await self._get_kwargs_from_page(page)
-        if content:
-            kwargs.setdefault("content", content)
-
-        self._update_labels(0)
-        self.message = await self.ctx.send(**kwargs, view=self, ephemeral=ephemeral)
-
-    @discord.ui.button(label="≪", style=discord.ButtonStyle.grey)
-    async def go_to_first_page(
-        self, interaction: discord.Interaction, button: discord.ui.Button
-    ):
-        """go to the first page"""
-        await self.show_page(interaction, 0)
-
-    @discord.ui.button(label="Back", style=discord.ButtonStyle.blurple)
-    async def go_to_previous_page(
-        self, interaction: discord.Interaction, button: discord.ui.Button
-    ):
-        """go to the previous page"""
-        await self.show_checked_page(interaction, self.current_page - 1)
-
-    @discord.ui.button(label="Current", style=discord.ButtonStyle.grey, disabled=True)
-    async def go_to_current_page(
-        self, interaction: discord.Interaction, button: discord.ui.Button
-    ):
-        pass
-
-    @discord.ui.button(label="Next", style=discord.ButtonStyle.blurple)
-    async def go_to_next_page(
-        self, interaction: discord.Interaction, button: discord.ui.Button
-    ):
-        """go to the next page"""
-        await self.show_checked_page(interaction, self.current_page + 1)
-
-    @discord.ui.button(label="≫", style=discord.ButtonStyle.grey)
-    async def go_to_last_page(
-        self, interaction: discord.Interaction, button: discord.ui.Button
-    ):
-        """go to the last page"""
-        # The call here is safe because it's guarded by skip_if
-        await self.show_page(interaction, self.source.get_max_pages() - 1)  # type: ignore
-
-    @discord.ui.button(label="Skip to page...", style=discord.ButtonStyle.grey)
-    async def numbered_page(
-        self, interaction: discord.Interaction, button: discord.ui.Button
-    ):
-        """lets you type a page number to go to"""
-        if self.message is None:
-            return
-
-        modal = NumberedPageModal(self.source.get_max_pages())
-        await interaction.response.send_modal(modal)
-        timed_out = await modal.wait()
-
-        if timed_out:
-            await interaction.followup.send("Took too long", ephemeral=True)
-            return
-        elif self.is_finished():
-            await modal.interaction.response.send_message(
-                "Took too long", ephemeral=True
-            )
-            return
-
-        value = str(modal.page.value)
-        if not value.isdigit():
-            await modal.interaction.response.send_message(
-                f"Expected a number not {value!r}", ephemeral=True
-            )
-            return
-
-        value = int(value)
-        await self.show_checked_page(modal.interaction, value - 1)
-        if not modal.interaction.response.is_done():
-            error = modal.page.placeholder.replace("Enter", "Expected")  # type: ignore # Can't be None
-            await modal.interaction.response.send_message(error, ephemeral=True)
-
-    @discord.ui.button(label="Quit", style=discord.ButtonStyle.red)
-    async def stop_pages(
-        self, interaction: discord.Interaction, button: discord.ui.Button
-    ):
-        """stops the pagination session."""
-        await interaction.response.defer()
-        await interaction.delete_original_response()
-        self.stop()
+if TYPE_CHECKING:
+    from bot import LunaBot
 
 
 class GroupHelpPageSource(menus.ListPageSource):
@@ -327,7 +59,10 @@ class GroupHelpPageSource(menus.ListPageSource):
 
 class HelpSelectMenu(discord.ui.Select["HelpMenu"]):
     def __init__(
-        self, entries: dict[commands.Cog, list[commands.Command]], bot, number: int = 0
+        self,
+        entries: dict[commands.Cog, list[commands.Command]],
+        bot: "LunaBot",
+        number: int = 0,
     ):
         if number == 0:
             placeholder = "Select a category..."
@@ -353,8 +88,8 @@ class HelpSelectMenu(discord.ui.Select["HelpMenu"]):
                 value="__index",
                 description="The help page showing how to use the bot.",
             )
-        for cog, commands in self.commands.items():
-            if not commands:
+        for cog, cmds in self.commands.items():
+            if not cmds:
                 continue
             description = cog.description.split("\n", 1)[0] or None
             emoji = getattr(cog, "display_emoji", None)
@@ -369,7 +104,8 @@ class HelpSelectMenu(discord.ui.Select["HelpMenu"]):
         assert self.view is not None
         value = self.values[0]
         if value == "__index":
-            await self.view.rebind(FrontPageSource(), interaction)
+            base_embed = self.bot.get_embed("help")
+            await self.view.rebind(FrontPageSource(base_embed), interaction)
         else:
             cog = self.bot.get_cog(value)
             if cog is None:
@@ -392,6 +128,10 @@ class HelpSelectMenu(discord.ui.Select["HelpMenu"]):
 
 
 class FrontPageSource(menus.PageSource):
+    def __init__(self, base_embed: discord.Embed | None):
+        self.base_embed = base_embed
+        super().__init__()
+
     def is_paginating(self) -> bool:
         # This forces the buttons to appear even in the front page
         return True
@@ -407,29 +147,11 @@ class FrontPageSource(menus.PageSource):
         return self
 
     def format_page(self, menu, page: Any):
-        embed = discord.Embed(
-            title="❀ㆍㆍBot Help﹗⁺ ₍ <a:LCD_Nikki_yay:1127849118992171058> ₎",
-            colour=0xCAB7FF,
-        )
-        embed.description = """** **                          ‧  ╴‧  ╴‧  ╴‧
-> ♡﹒﹒Hi hi, welcome to our help page; if you'd like to see all my available commands and how to use them, this is the right place to be! ✿❀
-                           ‧  ╴‧  ╴‧  ╴‧
-> ⁺﹒Use `!help command` for more info about a certain command﹒⁺
-> ⁺﹒Use `!help category` for more info about a certain category﹒⁺
-> ⁺﹒And use the dropdown menu below to select a category of commands to view﹒⁺
-                           ‧  ╴‧  ╴‧  ╴‧
-> ✿﹒*My home is this great server...*
-> <:LC_reply_F2U:1081275742765195355> https://discord.gg/lunalilac <a:Lumi_sparkles:899826759313293432> 
-                           ‧  ╴‧  ╴‧  ╴‧
-> ❀﹒*Wondering who I am??*
-> <:LC_reply_F2U:1081275742765195355> I'm the amazing mascot of all Moonlit Lilacs, but in bot form! I was coded by the epic `stormtorch`; you may see him around the server sometimes, likely saying random things like "ur mom." If you ever experience any bugs or have any concerns regarding me, he is the one you need to contact.
-                           ‧  ╴‧  ╴‧  ╴‧
-> <a:Lumi_purple_exclamation:899739130215411792> ﹒ ﹒ Here's a heads up ﹒ ﹒ <a:Lumi_purple_exclamation:899739130215411792>
-> <:LC_reply_F2U:1081275742765195355> You __cannot__ add me to your own servers! I am solely here to serve Moonlit Lilac servers. Please do not ask, nor attempt to add me elsewhere.
-                           ‧  ╴‧  ╴‧  ╴‧"""
-        embed.set_footer(
-            text="⁺﹒This help command was borrowed from Danny's bot R. Danny﹒⁺"
-        )
+        if self.base_embed is None:
+            embed = discord.Embed(title="Placeholder Help Embed")
+        else:
+            embed = self.base_embed.copy()
+
         if self.index == 1:
             entries = (
                 ("<argument>", "This means the argument is __**required**__."),
@@ -555,7 +277,8 @@ class PaginatedHelpCommand(commands.HelpCommand):
             assert cog is not None
             all_commands[cog] = sorted(children, key=lambda c: c.qualified_name)
 
-        menu = HelpMenu(FrontPageSource(), ctx=self.context)
+        base_embed = self.context.bot.get_embed("help")
+        menu = HelpMenu(FrontPageSource(base_embed), ctx=self.context)
         menu.add_categories(all_commands)
         await menu.start()
 
